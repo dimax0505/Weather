51d222422cae3a69b4902831406410817df90180
dimax0505 <dimax0505@gmail.com>
2018-10-15 14:53:07 +0300

    Вывод на экран сообщений жизненного цикла Activity

--- app/src/main/java/com/example/maximovd/weather/MainActivity.java
+++ app/src/main/java/com/example/maximovd/weather/MainActivity.java
@@ -6,14 +6,81 @@ package com.example.maximovd.weather;
 //        4. *Внесите изменения в приложение LifeCycle, чтобы сообщения о жизненном цикле выводились не только всплывающими сообщениями, но и в logcat.
 
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+     private final String bundlekey = "bundle_key";
+     TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+
+
+        String instantState;
+        if (savedInstanceState == null)
+            instantState = getString(R.string.first_launch);
+        else instantState = getString(R.string.relaunch);
+
+        Toast.makeText(getApplicationContext(),instantState + " on Create", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+         Toast.makeText(getApplicationContext(),"on Start", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Toast.makeText(getApplicationContext(),"on RestoreInstanceState -  Повторный запуск.  " +
+                "textView: " + savedInstanceState.getString(bundlekey,""), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+       Toast.makeText(getApplicationContext(),"on Resume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(),"on Pause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString(bundlekey, textView.getText().toString());
+        super.onSaveInstanceState(outState);
+        Toast.makeText(getApplicationContext(),"on SaveInstanceState", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(),"on Stop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(),"on Restart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(),"on Destroy", Toast.LENGTH_SHORT).show();
     }
 }
