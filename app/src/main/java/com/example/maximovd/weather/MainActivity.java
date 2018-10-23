955c022e4431034c6ac94d9f25aef25cc031de0a
dimax0505 <dimax0505@gmail.com>
2018-10-23 17:12:12 +0300

    сделал дз с чекбоксами и свитчем, для получения погодных данных завел класс-заглушку, отдающий данные по трем городам москва, лондон и париж

--- app/src/main/java/com/example/maximovd/weather/MainActivity.java
+++ app/src/main/java/com/example/maximovd/weather/MainActivity.java
@@ -3,11 +3,8 @@ package com.example.maximovd.weather;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telecom.TelecomManager;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.*;
 
 //1. Создайте два экрана в вашем приложении. На первом экране создайте форму запроса для города (EditText) и кнопку отправки запроса.
 // По нажатию на кнопку открывайте второй экран, передавайте в него название города с предыдущего экрана и выводите данные о погоде вместе с названием города.
@@ -19,6 +16,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private EditText name_of_city;
     private Button ok;
+    private Switch switch_addition;
+    private CheckBox checkBox_Humidity, checkBox_Speed, checkBox_Pressure;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
         initViews();
         setOnOkButton();
+        setOnCheckBox();
 
 
     }
@@ -34,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
     private void initViews() {
        name_of_city = findViewById(R.id.name_of_city);
        ok = findViewById(R.id.batton_Ok);
+       switch_addition = findViewById(R.id.switch_addition);
+       checkBox_Humidity = findViewById(R.id.humidity);
+       checkBox_Speed = findViewById(R.id.speed_of_wind);
+        checkBox_Pressure = findViewById(R.id.pressure);
+
     }
 
     private void setOnOkButton (){
@@ -43,8 +48,30 @@ public class MainActivity extends AppCompatActivity {
                String city = name_of_city.getText().toString();
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 intent.putExtra("name_of_city", city);
+                intent.putExtra("humidity", checkBox_Humidity.isChecked());
+                intent.putExtra("speed_of_wind", checkBox_Speed.isChecked());
+                intent.putExtra("pressure", checkBox_Pressure.isChecked());
                 startActivity(intent);
 
+            }
+        });
+    }
+
+    private void setOnCheckBox() {
+        switch_addition.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (switch_addition.isChecked()){
+                    checkBox_Humidity.setVisibility(View.VISIBLE);
+                    checkBox_Speed.setVisibility(View.VISIBLE);
+                    checkBox_Pressure.setVisibility(View.VISIBLE);
+                }
+                else {
+                    checkBox_Humidity.setVisibility(View.INVISIBLE);
+                    checkBox_Speed.setVisibility(View.INVISIBLE);
+                    checkBox_Pressure.setVisibility(View.INVISIBLE);
+                }
             }
         });
     }
